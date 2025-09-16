f55f1e29756337cb402f603194881f0bb76f76a2
TenshiRepo <lorenzraymundo08@gmail.com>
2025-09-16 17:57:54 +0800

    Add files via upload

--- SW1/Currency_converter.py
+++ SW1/Currency_converter.py
@@ -1,1 +1,24 @@
+while True:
+    print("Enter dollar ($) (* to exit):")
+    str = input()
+
+    if str == '*':
+        print("Bye")
+        break
+
+    strlist = str.split('@')
+
+    mytuple = tuple(int(elements) for elements in strlist)
+
+    def convert(x):
+        rupees = x*88.15
+        pound = x*0.74
+        yuan = x*7.12
+        return (rupees, pound, yuan)
+
+    print(f"{'Dollar ($)':<12} {'Indian Rupee (R)':<20} {'British (Pound)':<20} {'China (Y)':<20}")
+
+    for i in mytuple:
+        converted = convert(i)
+        print(f"{i:<12} {converted[0]:<20} {converted[1]:<20} {converted[2]:<20}")
 
